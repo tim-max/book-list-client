a6dc6a58e51751322f024cdb14443b96a39770fc
Tim McCoy <timmymccoy@gmail.com>
2018-04-25 11:08:13 -0700

    added additional directory, worked on book.js

--- scripts/models/book.js
+++ scripts/models/book.js
@@ -1,6 +1,29 @@
-let ENV = {
-    currentUrl: "https://git.heroku.com/tm-mr-booklist.git",
-    cloudApi: ,
-    localApi: ,
-    setApi: 
-}
+
+
+const ENV = {}
+
+    
+    ENV.currentUrl = window.location.protocol === 'https:';
+    ENV.cloudApi = "https://git.heroku.com/tm-mr-booklist.git";
+    ENV.localApi = 'http://localhost:3000';
+    ENV.setApi = ENV.currentUrl ? ENV.cloudApi : ENV.localApi; 
+
+Books.prototype.toHtml = function(){
+    let template = Handlebars.compile($("#books-detail-template").text());
+    return template(this);
+};
+    
+Books.add = book => {
+    $.post(`${ENV.cloudApi}/api/v1/books`)
+    .then(Books.loadAll)
+    .then(callback)
+    .catch(errorCallback);
+};
+
+Books.fetchOne = callback => {
+    $.get(`${ENV.apiUrl}/api/v1/books/:id`)
+    .then(Books.loadAll)
+    .then(callback)
+    .catch(errorCallback);
+};
+
